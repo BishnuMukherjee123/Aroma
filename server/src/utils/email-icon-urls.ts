23228d423b0f39cdb7542527c00c70d587147529
Bishnu Mukherjee <jerry.bs12345@gmail.com>
2026-06-02 01:46:32 +0530

    fix(email): replace base64 icons with hosted ImageKit CDN URLs

---
+++ server/src/utils/email-icon-urls.ts
@@ -0,0 +1,11 @@
+/**
+ * Email icon URLs hosted on ImageKit CDN.
+ * These are used in the welcome email template.
+ * To re-generate/re-upload: node scratch/upload-email-icons.js
+ */
+export const EMAIL_ICONS = {
+  menus: "https://ik.imagekit.io/3tbsb6ckk/email-icons/email-icon-menus.png",
+  qr:    "https://ik.imagekit.io/3tbsb6ckk/email-icons/email-icon-qr.png",
+  team:  "https://ik.imagekit.io/3tbsb6ckk/email-icons/email-icon-team.png",
+  ar:    "https://ik.imagekit.io/3tbsb6ckk/email-icons/email-icon-ar.png",
+} as const;
